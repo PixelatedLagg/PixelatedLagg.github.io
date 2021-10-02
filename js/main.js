aa46f25ff79d917ffbe4219ee380227046f15c10
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2021-10-02 12:24:23 -0400

    added some js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function onhover(obj) {
+    obj.style.height = "60px";
+    obj.style.width = "60px";
+}
+function onhoverexit(obj) {
+    obj.style.height = "50px";
+    obj.style.width = "50px";
+}

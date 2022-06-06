67efa15749b9be252592c185b6b43e323f3539bc
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-06-05 23:50:39 -0400

    add: website overhaul

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 $(function () {
     $('#form').on('submit',function (e) {
         e.preventDefault();
-        terminalInput();
+        input(document.getElementById("input").value);
     });
 });
-function terminalInput()
+function input(input)
 {
-    if (document.getElementById("input").value == "")
+    if (input == "")
     {
         return;
     }
-    if (document.getElementById("input").value == "clear")
+    if (input == "clear" || input == "cls")
     {
         $('#output').empty();
         document.getElementById("input").value = "";
@@ -18,10 +18,11 @@ function terminalInput()
     }
     var p = document.createElement('p');
     var inp = document.createElement('p');
-    switch (document.getElementById("input").value)
+    switch (input)
     {
         case "info":
-            p.innerHTML = "\"projects\"   - Display all of my projects, new and old.<br>\"old\"   - Go back to my old website (The one that isn't a terminal).<br>\"about\"   - Learn more about me.<br>\"clear\"   - Clear the terminal.";
+        case "help":
+            p.innerHTML = "<span onclick=\"input('projects')\">projects</span>   - Display all of my projects, new and old.<br><span onclick=\"input('old')\">old</span>   - Go back to my old website (The one that isn't a terminal).<br><span onclick=\"input('about')\">about</span>   - Learn more about me.<br><span onclick=\"input('clear')\">clear</span>   - Clear the terminal.";
             break;
         case "projects":
             p.innerHTML = "<a href=\"https://pixelated-lagg.itch.io/jump-squared\">Jump Squared</a><br>A Jump Squared is a basic 2d platformer based on Geometry Dash and Jump King. <br>It is centered around a nameless and emotionless blue square who needs to get to the white platform at the end of each level. (Very original, I know.)<br> Currently, there are 3 levels in the game, and I am planning to eventually finish the game with a fourth level.<br><br><a href=\"https://github.com/PixelatedLagg/Impart\">Impart</a><br>Impart is a library of C# methods to help developers in creating a cool looking website, while skipping the annoying markup. Somewhat ironcially, this website isn't made using it. <br>When Impart evolves enough, I will have an example website (with source code) for all to admire.<br><br><a href=\"https://github.com/PixelatedLagg/Scrape\">Scrape</a><br>Scrape is an object oriented programming language very similiar to C#, in syntax and in use. <br>The fellow developers and I plan to implement a default framework to rival C#'s System.Reflections and a dynamic naming operator to allow for maximum variable control for the developer.";
@@ -33,20 +34,14 @@ function terminalInput()
             p.innerHTML = "<a href=\"https://pixelatedlagg.github.io/old-website/\">Old Website</a>";
             break;
         case "welcome":
-            p.innerHTML = "You have already been welcomed :/";
-            break;
-        case "wwssadadba":
-            p.innerHTML = "You have found my hidden secret :>";
+            p.innerHTML = "Welcome to my website. Use <span onclick=\"input('ui')\">ui</span> to be sent to a normal user interface. Use <span onclick=\"input('help')\">help</span> to list all commands.";
             break;
         default:
-            p.innerHTML = `"${document.getElementById("input").value}" is not a recognized command!`;
+            p.innerHTML = `${input} : The term '${input}' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is correct and try again.`;
             break;
     }
-    inp.innerHTML = `<i>root@pl-website</i>:<b>~</b>$ ${document.getElementById("input").value}`;
+    inp.innerHTML = `<i>root@pl-website</i>:<b>~</b>$ ${input}`;
     document.getElementById("output").appendChild(inp);
     document.getElementById("output").appendChild(p);
     document.getElementById("input").value = "";
-}
-function removeItem(item) {
-    item.remove();
 }

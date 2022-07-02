0e2e4583f7e91ecefd9c9e35142e4e74ff8536d0
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-07-02 13:05:20 -0400

    add: starting better ui

--- ui/index.js
+++ ui/index.js
@@ -1,10 +1,4 @@
-var text = document.getElementById("text");
-
-function input(arg) {
-    switch (arg)
-    {
-        case "csharp":
-            text.innerHTML = "Pixelated is very skilled in C#. Having used it for about 2 years, they can create basically anything with it."
-            break;
-    }
+function DetailClick()
+{
+    console.log('aids!');
 }

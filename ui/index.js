1f278e13246dd490041790b72ed1f8f57b1d6b2d
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-02-11 17:56:32 -0500

    more responsiveness

---
+++ ui/index.js
@@ -0,0 +1,22 @@
+window.addEventListener('resize', function() //resize moving text on zoom
+{
+    if (window.innerHeight > 948)
+    {
+        document.documentElement.style.setProperty("--sideWidth", window.innerHeight + "px");
+    }
+});
+
+fetch('https://pixelatedlagg.github.io/projects.json').then(response => response.json()).then(data => //read projects file and put as html
+{
+    var main = document.getElementsByClassName("main")[0];
+    //handle data
+    for (var project in data.projects)
+    {
+        main.innerHTML += `<div class="title"><h2>${project.title}</h2><a href="${project.link}">${project.symbol}</a></div><p>${project.description}</p>`;
+    }
+    console.log(data);
+})
+.catch(error =>
+{
+    console.error('Error reading JSON file:', error);
+});

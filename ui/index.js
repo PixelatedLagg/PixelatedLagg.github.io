cd9376083e4f75060fe0dd319bc2ae0e7e14498c
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-02-11 18:22:44 -0500

    fixed text

--- ui/index.js
+++ ui/index.js
@@ -1,19 +1,20 @@
-window.addEventListener('resize', function() //resize moving text on zoom
+resizeUpdate();
+function resizeUpdate()
 {
     if (window.innerHeight > 948)
     {
         document.documentElement.style.setProperty("--sideWidth", window.innerHeight + "px");
     }
-});
+}
+window.addEventListener('resize', resizeUpdate()); //resize moving text on zoom
 
 fetch('https://pixelatedlagg.github.io/projects.json').then(response => response.json()).then(data => //read projects file and put as html
 {
     var main = document.getElementsByClassName("main")[0];
     //handle data
-    for (var project in data.projects)
-    {
+    data.projects.forEach(project => {
         main.innerHTML += `<div class="title"><h2>${project.title}</h2><a href="${project.link}">${project.symbol}</a></div><p>${project.description}</p>`;
-    }
+    });
     console.log(data);
 })
 .catch(error =>

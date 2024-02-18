42a23ae12fc5d095a540f6b61072f18a275038bf
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-02-17 23:53:09 -0500

    projects json in terminal

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var projectsText = "";
+
+fetch('https://pixelatedlagg.github.io/projects.json').then(response => response.json()).then(data => //read projects file and put as html
+{
+    //handle data
+    data.projects.forEach(project => {
+        //main.innerHTML += `<div class="title"><h2>${project.title}</h2><a href="${project.link}">${project.symbol}</a></div><p>${project.description}</p>`;
+        projectsText += `<a href=\"${project.link}\">${project.title}</a><br>${project.description}<br><br>`;
+    });
+    projectsText = projectsText.slice(0, -8);
+})
+.catch(error =>
+{
+    console.error('Error reading JSON file:', error);
+});
+
 window.onpopstate = function() { //force reload on chromium
     location.reload();
 }
@@ -32,7 +48,7 @@ function input(input)
             p.innerHTML = "<span onclick=\"input('projects')\">projects</span>   - Display all of my projects, new and old.<br><span onclick=\"input('old')\">old</span>   - Go back to my old website (The one that isn't a terminal).<br><span onclick=\"input('about')\">about</span>   - Learn more about me.<br><span onclick=\"input('clear')\">clear</span>   - Clear the terminal.";
             break;
         case "projects":
-            p.innerHTML = "<a href=\"https://pixelated-lagg.itch.io/jump-squared\">Jump Squared</a><br>A Jump Squared is a basic 2d platformer based on Geometry Dash and Jump King. <br>It is centered around a nameless and emotionless blue square who needs to get to the white platform at the end of each level. (Very original, I know.)<br> Currently, there are 3 levels in the game, and I am planning to eventually finish the game with a fourth level.<br><br><a href=\"https://github.com/PixelatedLagg/Impart\">Impart</a><br>Impart is a library of C# methods to help developers in creating a cool looking website, while skipping the annoying markup. Somewhat ironcially, this website isn't made using it. <br>When Impart evolves enough, I will have an example website (with source code) for all to admire.<br><br><a href=\"https://github.com/PixelatedLagg/Scrape\">Scrape</a><br>Scrape is an object oriented programming language very similiar to C#, in syntax and in use. <br>The fellow developers and I plan to implement a default framework to rival C#'s System.Reflections and a dynamic naming operator to allow for maximum variable control for the developer.";
+            p.innerHTML = projectsText;
             break;
         case "about":
             p.innerHTML = "Hello! I'm PixelatedLagg.<br>I am a teenage developer who likes to make games, applications, and discord bots. <br>I mainly use C#, however I am learning C and C++. I am also somewhat competent in HTML/CSS. <br>With nearly 2 years of experience and way too much free time, I am constantly making a new game or piece of software.<br><a href=\"https://discord.gg/4W5rTDD73X\">Discord</a> <a href=\"https://github.com/PixelatedLagg\">Github</a> <a href=\"https://itch.io/profile/pixelated-lagg\">Itch</a>";
